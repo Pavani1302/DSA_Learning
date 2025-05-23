c60620c19334ca4472a0148661000db0332d38ce
Pavani1302 <pavaniboorna@gmail.com>
2025-05-23 19:17:37 +0530

    Group Anagrams

---
+++ 3.Hashing/2.groupingAnagrams.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+def anagram(strs):
+    strs_dict = defaultdict(list) 
+    result = []
+    for s in strs:
+        sorted_s = tuple(sorted(s))
+        strs_dict[sorted_s].append(s)
+    for value in strs_dict.values():
+        result.append(value)
+    return result 
+
+print(anagram(["eat","tea","tan","ate","nat","bat"]))
