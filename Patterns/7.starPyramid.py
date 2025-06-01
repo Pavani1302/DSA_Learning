d5c44d2398c6b1d42cc2a8ca7c7f6d4b76a19a10
Pavani1302 <pavaniboorna@gmail.com>
2025-06-01 12:33:00 +0530

    Star Pyramid

--- Patterns/7.starPyramid.py
+++ Patterns/7.starPyramid.py
@@ -1,5 +1,9 @@
 def starPyramid(n):
-    pass
-
+    for rows in range(1,n+1):
+        for spaces in range(n - rows):
+            print(" ", end="")
+        for stars in range(1, 2 * rows):
+            print("*", end="")
+        print()
 n = int(input("Enter n: "))
-print(starPyramid(n))
+starPyramid(n)
